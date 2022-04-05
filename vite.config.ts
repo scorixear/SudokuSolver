2a586c0daf8e804e3be4339125bf941e4a60f4b3
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2022-04-05 16:09:12 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "/sudokusolver",
+  base: "",
   plugins: [vue()],
   resolve: {
     alias: {
